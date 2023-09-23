a2d846fbc95cb47072de977b39676258e8b215ec
Ivan Bainotti <ivan.bainotti@mi.unc.edu.ar>
2023-09-23 12:40:43 -0300

    Agregado modelo de datos

--- Database/Database.py
+++ Database/Database.py
@@ -9,5 +9,44 @@ db = pony.orm.Database()
 if "pytest" in sys.modules:
     db.bind(provider='sqlite', filename=':sharedmemory:')
 else:
-    db.bind(provider='sqlite', filename='db.lacosa', create_db=True)
+    db.bind(provider='sqlite', filename='lacosa.sqlite', create_db=True)
+
+class Match(db.Entity):
+    name = PrimaryKey(str)
+    password = Optional(str, default="")
+    min_players = Required(int)
+    max_players = Required(int)
+    players = Set("Player")
+    initiated = Optional(bool, default=False)
+    clockwise = Optional(bool, default=True)
+    current_player = Optional(int, default=0)
+    deck = Set("Deck")
+
+class Player(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str, unique=True)
+    match = Optional(Match)
+    is_host = Optional(bool, default=False)
+    cards = Set("Card")
+    position = Optional(int)
+    rol = Optional(int)
+    is_alive = Optional(bool)
+
+class Card(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    type = Required(int)
+    description = Required(str)
+    number = Required(int)
+    player = Optional(Player)
+    deck = Optional("Deck")
+
+class Deck(db.Entity):
+    match = Required(Match)
+    cards = Set(Card)
+    is_discard = Required(bool)
+    PrimaryKey(match, is_discard)
+
+
+db.generate_mapping(create_tables=True)
 
