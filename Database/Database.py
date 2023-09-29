7dac667cf6d60a0fc9d0c3320be7d8cbccc852eb
Ivan Bainotti <ivan.bainotti@mi.unc.edu.ar>
2023-09-29 19:02:25 -0300

    Implementado endpoint robar carta
    Faltan los tests

--- Database/Database.py
+++ Database/Database.py
@@ -12,7 +12,8 @@ else:
     db.bind(provider='sqlite', filename='lacosa.sqlite', create_db=True)
 
 class Match(db.Entity):
-    name = PrimaryKey(str)
+    id = PrimaryKey(int, auto=True)
+    name = Required(str, unique=True)
     password = Optional(str, default="")
     min_players = Required(int)
     max_players = Required(int)
@@ -50,3 +51,64 @@ class Deck(db.Entity):
 
 db.generate_mapping(create_tables=True)
 
+
+@db_session
+def _get_player(player_id: int) -> Player:
+    if not Player.exists(id=player_id):
+        raise Exception("Player not found")
+    return Player[player_id]
+
+@db_session
+def _get_match(match_id: int) -> Match:
+    if not Match.exists(id=match_id):
+        raise Exception("Match not found")
+    return Match[match_id]
+
+@db_session
+def get_match_turn(match_id: int) -> int:
+    match = _get_match(match_id)
+    return match.current_player
+
+@db_session
+def is_player_alive(player_id: int) -> bool:
+    player = _get_player(player_id)
+    return player.is_alive
+
+@db_session
+def get_player_position(player_id: int) -> int:
+    player = _get_player(player_id)
+    return player.position
+
+@db_session
+def is_deck_empty(match_id: int) -> bool:
+    match = _get_match(match_id)
+    deck = Deck.get(match=match, is_discard=False)
+    return len(deck.cards) == 0
+
+@db_session
+def get_player_match(player_id:int ) -> int:
+    player = _get_player(player_id)
+    if not player.match:
+        raise Exception("Player not in a match")
+    return player.match.id
+
+@db_session
+def new_deck_from_discard(match_id: int):
+    """ Pre: The match is initialized """
+    match = _get_match(match_id)
+    discard_deck = Deck.get(match=match, is_discard=True)
+    deck = Deck.get(match=match, is_discard=False)
+    deck.cards = discard_deck.cards.copy()
+    discard_deck.cards.clear()
+    
+
+@db_session
+def pick_random_card(player_id: int) -> int:
+    """ Pre: The deck is not empty """
+    player = _get_player(player_id)
+    deck = Deck.get(match=player.match, is_discard=False)
+    card = deck.cards.random(1)[0]
+    card.player = player
+    card.deck = None
+    deck.cards.remove(card)
+    return card.id
--- app.py
+++ app.py
@@ -35,3 +35,26 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
+
+
+@app.post("/match/deck/pickup", tags=["Cards"], status_code=status.HTTP_200_OK)
+def pickup_card(player_id: int):
+    """
+    The player get a random card from the deck and add it to his hand
+    if the deck is empty, form a new deck from the discard deck
+    """
+    try:
+        match_id = get_player_match(player_id)
+        turn = get_match_turn(match_id)
+        player_position = get_player_position(player_id)
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    if not is_player_alive(player_id):
+        return {"status": "error", "message": "Player is dead"}
+    elif turn != player_position:
+        return {"status": "error", "message": "Not player turn"}
+    
+    if is_deck_empty(match_id):
+        new_deck_from_discard(match_id)
+    card_id = pick_random_card(player_id)
+    return {"status": "ok", "card_id": card_id}
