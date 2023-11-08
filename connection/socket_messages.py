d52defe79adcac99e94670ffc0daae75e5cb4de5
Esteban Druetta <esteban.druetta@mi.unc.edu.ar>
2023-11-08 19:12:51 -0300

    Se restauro mensaje CHAT_NOTIFICATION

--- connection/socket_messages.py
+++ connection/socket_messages.py
@@ -30,6 +30,7 @@ OBSTACLES = "obstáculos"
 QUARANTINE = "cuarentena"
 DEFENSE_STAMP = "timestamp"
 DIRECTION = "sentido horario"
+CHAT_RECORD = "historial"
 CHAT_NOTIFICATION = "notificación chat"
 
 # ------ Auxiliary functions for sockets messages ------
